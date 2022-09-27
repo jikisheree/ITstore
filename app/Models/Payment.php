54417b7b8fe3a4ec16bae759b7ba2dbb3bb41888
latthaphol laohapiboonrattana <work21_123@hotmail.com>
2022-09-27 14:11:37 +0700

    remove pid

--- app/Models/Payment.php
+++ app/Models/Payment.php
@@ -8,5 +8,5 @@ use Illuminate\Database\Eloquent\Model;
 class Payment extends Model
 {
     use HasFactory;
-    protected $fillable= ['UID','PID', 'amount','PaymentDate'];
+    protected $fillable= ['UID', 'amount','PaymentDate'];
 }
