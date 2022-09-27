34a30cd7dc07f70f0d3282551755a41b34693562
latthaphol laohapiboonrattana <work21_123@hotmail.com>
2022-09-27 14:42:42 +0700

    add categoryseeder

--- database/seeders/CategorySeeder.php
+++ database/seeders/CategorySeeder.php
@@ -1,44 +1,47 @@
 <?php
+
 namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
-use App\Models\Product;
+use App\Models\Category;
 class CategorySeeder extends Seeder
 {
-/**
-* Run the database seeds.
-*
-* @return void
-*/
-public function run()
-{
-$categoies = [
-[
-    'categoryID' => '1',
-    'categoryName' => 'Monitor'
-],
-[
-    'categoryID' => '2',
-    'categoryName' => 'Mouse'
-],
-[
-    'categoryID' => '3',
-    'categoryName' => 'Keyboard'
-],
-[
-    'categoryID' => '4',
-    'categoryName' => 'Headset'
-],
-[
-    'categoryID' => '5',
-    'categoryName' => 'Microphone'
-],
-[
-    'categoryID' => '6',
-    'categoryName' => 'Speaker'
-],
-];
-foreach ($products as $key => $value) {
-Product::create($value);
-}
-}
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $_category = [
+            [
+               
+                'categoryName' => 'Monitor'
+            ],
+            [
+               
+                'categoryName' => 'Mouse'
+            ],
+            [
+                
+                'categoryName' => 'Keyboard'
+            ],
+            [
+               
+                'categoryName' => 'Headset'
+            ],
+            [
+               
+                'categoryName' => 'Microphone'
+            ],
+            [
+               
+                'categoryName' => 'Speaker'
+            ],
+            ];
+            foreach ($_category as $key => $value) {
+            Category::create($value);
+            }
+    }
 }
