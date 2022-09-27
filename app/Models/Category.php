4d3dd50c62ee52a9f488b4542918f031c8b4f383
latthaphol laohapiboonrattana <work21_123@hotmail.com>
2022-09-27 14:11:16 +0700

    remove stock

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -8,5 +8,5 @@ use Illuminate\Database\Eloquent\Model;
 class Category extends Model
 {
     use HasFactory;
-    protected $fillable= ['categoryID','categoryName','stock'];
+    protected $fillable= ['categoryID','categoryName'];
 }
