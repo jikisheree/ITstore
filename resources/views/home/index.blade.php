2dd14cd070228f2d39c7879393e83e1e0ba6f6ad
lalinthorn <lalinpye@gmail.com>
2022-11-03 22:17:21 +0700

    webpage

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -2,37 +2,67 @@
 @section('content')
 
 <head>
-    <link rel="stylesheet" href="/resources/css/homepage.css">
+    <link rel="stylesheet" type="text/css" href="/resources/css/homepage.css" media=”screen” />
     <style>
         @import url('https://fonts.googleapis.com/css2?family=PT+Serif:ital@1&family=Prompt&display=swap');
 
         .tx-head {
-            font-family: 'Didot';
+            font-family: Gemini Moon Regular;
             font-size: 70px;
-            font-weight: 400;
+            font-weight: 500;
             line-height: 100px;
-            color: rgb(77, 95, 152);
+            color: #4773EB;
+            text-align: center;
+            padding: bottom 15px; ;
         }
 
         .text {
+            font-family: 'Didot';
+            font-family: 'Prompt', 'Roboto Slab';
+            font-size: 20px;
+            font-weight: 300;
+            line-height: 25px;
+
+        }
+
+        .ttext {
             font-family: 'PT Serif', serif;
+            font-size: 20px;
+            font-weight: 400;
+            line-height: 30px;
+        }
+
+        .box:hover {
+            box-shadow: 0 0 2px 1px rgba(0, 140, 186, 0.5);
+        }
+
+        strong {
+            color: black;
+            font-size: 20px;
         }
 
         p {
             font-family: 'Prompt', 'Roboto Slab';
             color: rgb(0, 0, 0);
+            font-size: 13px;
         }
 
-        h4{
-            font-size: 30px; 
-            font-weight: 400; 
-            line-height: 30px; 
+        h4 {
+            font-size: 20px;
+            font-weight: 500px;
+            line-height: 30px;
             color: rgb(77, 95, 152);
 
         }
+
+        .clearfix::after {
+            content: "";
+            clear: both;
+            display: table;
+        }
     </style>
 </head>
-<div class="p-5 rounded" style="background-color: rgb(245, 245, 245)">
+<div class="p-5 rounded" style="background-color:#eaeaf4 ">
     @auth
     <h1>Products</h1>
     <p class="lead">Choose the product that you want to buy.</p>
@@ -59,20 +89,20 @@
     @endauth
     @guest
     <h1 class="tx-head">Homepage</h1>
-    <p class="lead text">You have to login before buy the products.</p>
+    <p class="text" style="text-shadow: 2px 2px red; font-weight: 700;">You have to login before buy the products.</p>
     <div class="flex-center position-ref full-height">
         <div class="content">
             <p class="text">What we have in stocks.</p>
             <div class="row">
                 @foreach($products as $product)
                 <div class="col-xs-18 col-sm-6 col-md-3">
-                    <div style="padding: 25px; border-radius: 8px; border-bottom: 6px solid red; background-color: white; height: max-height; width: 350px; ">
-                        <img src="{{ $product->image }}" alt="" style="width: 300px;">
+                    <div class="clearfix" style="padding: 25px; border-radius: 8px; border-bottom: 6px solid red; background-color: white; height: max-height; width: 350px; box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);">
+                        <img src="{{ $product->image }}" style="width: 300px;">
                         <div class="caption">
-                            <h4>{{ $product->name }}</h4>
-                            <p class="overflow-md-auto th-text" style="font-size: 13px">{{ $product->description }}</p>
-                            <p style="color: Gray"><strong style="color: black; font-size: 20px; ">Amount : </strong>{{ $product->stock }}</p>
-                            <p style="color: Tomato"><strong style="color: black; font-size: 20px;">Price : </strong> {{ $product->price }} $</p>
+                            <h4>{{ $product->Pname }}</h4>
+                            <p class="overflow-md-auto">{{ $product->description }}</p>
+                            <p style="color: Gray; font-size: 18px;"><strong>Amount : </strong>{{ $product->stock }}</p>
+                            <p style="color: Tomato; font-size: 18px;"><strong>Price : </strong> {{ $product->price }} $</p>
                         </div>
                     </div>
                 </div>
