966b62d6e2890002c0585970ca62ab5126a655b9
Siriwat Songwattana <pan-191260@hotmail.com>
2022-11-06 22:31:14 +0700

    navbar

--- resources/views/layouts/partials/navbar.blade.php
+++ resources/views/layouts/partials/navbar.blade.php
@@ -1,60 +1,47 @@
-<header class="the-main-header">
-    <div class="header-container">
-        <div class="container container-xl">
-            <section class="header-left">
-                <a href="/" class="site-logo nuxt-link-exact-active nuxt-link-active" aria-current="page">
-                    <img src="https://cdn-icons-png.flaticon.com/512/25/25694.png" width="50" height="50" alt="Site logo" class="img">
-                </a>
-                <div class="search-box-desktop search-box -bar">
-                    <div class="search-box-container">
-                        <div class="search-bar">
-                            <div class="text-field input-search-container">
-                                <div class="text-field-container -icon-right">
-                                    <input type="search" maxlength="125" placeholder="ค้นหาสินค้าที่ต้องการที่นี่....." class="input-search">
-                                </div>
-                            </div>
-                            <button class="btn -primary btn-search">
-                                <svg aria-hidden="true" focusable="false" data-prefix="fal" data-icon="search" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" class="icon svg-inline--fa fa-search fa-w-16">
-                                    <path fill="currentColor" d="M508.5 481.6l-129-129c-2.3-2.3-5.3-3.5-8.5-3.5h-10.3C395 312 416 262.5 416 208 416 93.1 322.9 0 208 0S0 93.1 0 208s93.1 208 208 208c54.5 0 104-21 141.1-55.2V371c0 3.2 1.3 6.2 3.5 8.5l129 129c4.7 4.7 12.3 4.7 17 0l9.9-9.9c4.7-4.7 4.7-12.3 0-17zM208 384c-97.3 0-176-78.7-176-176S110.7 32 208 32s176 78.7 176 176-78.7 176-176 176z" class=""></path>
+<header class="p-3 bg-dark text-white">
+    <div class="container">
+        <div class="d-flex flex-wrap align-items-center justify-content-center justify-content-lg-start">
+            <a href="/" class="d-flex align-items-center mb-2 mb-lg-0 text-white text-decoration-none">
+                <svg class="bi me-2" width="40" height="32" role="img" aria-label="Bootstrap">
+                    <use xlink:href="#bootstrap"/>
+                </svg>
+            </a>
+            <ul class="nav col-12 col-lg-auto me-lg-auto mb-2 justify-content-center mb-md-0">
+                <li><a href="/" class="nav-link px-2 text-white">Home</a></li>
+                <li><a href="/" class="nav-link px-2 text-white">Features</a></li>
+                <li><a href="/" class="nav-link px-2 text-white">Pricing</a></li>
+                <li><a href="/" class="nav-link px-2 text-white">FAQs</a></li>
+                <li><a href="/" class="nav-link px-2 text-white">About</a></li>
+            </ul>
+            @auth
+            {{auth()->user()->name}}
+            <button type="button" class="btn btn-info">
+                <a href="{{ url('cart') }}" class="btn btn-shopping-cart">Cart</a>
+            </button>
+            <form class="col-12 col-lg-auto mb-3 mb-lg-0 me-lg-3">
+                <div class="input-group">
+                    <input type="search" class="form-control" placeholder="Search for a product" aria-label="Search">
+                    <div class="input-group-append">
+                        <button type="submit" name="searchSubmit" value="Search" id="searchImage" class="btn btn-outline-secondary bg-white">
+                            <svg-icon name="megnifier-icon" class="btn-icon ng-star-inserted">
+                                <svg width="13" height="13" viewBox="0 0 13 13" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
+                                    <path fill-rule="evenodd" clip-rule="evenodd" d="M8.49254 5.30783C8.49254 7.0667 7.0667 8.49254 5.30783 8.49254C3.54897 8.49254 2.12313 7.0667 2.12313 5.30783C2.12313 3.54897 3.54897 2.12313 5.30783 2.12313C7.0667 2.12313 8.49254 3.54897 8.49254 5.30783ZM8.23489 9.73633C7.39581 10.292 6.38962 10.6157 5.30783 10.6157C2.3764 10.6157 0 8.23927 0 5.30783C0 2.3764 2.3764 0 5.30783 0C8.23927 0 10.6157 2.3764 10.6157 5.30783C10.6157 6.3897 10.292 7.39597 9.73621 8.23509L12.6889 11.1878C13.1035 11.6023 13.1035 12.2745 12.6889 12.6891C12.2743 13.1036 11.6022 13.1036 11.1876 12.6891L8.23489 9.73633Z"></path>
                                 </svg>
-                            </button>
-                        </div>
+                            </svg-icon>
+                        </button>
                     </div>
                 </div>
-            </section>
-            <section class="header-right">
-                @auth
-                {{auth()->user()->name}}
-                <a href="{{ route('logout.perform') }}">
-                    <button class="header-menu-item -with-text">
-                        <svg aria-hidden="true" focusable="false" data-prefix="fal" data-icon="user" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512" class="icon svg-inline--fa fa-user fa-w-14">
-                            <path fill="currentColor" d="M313.6 288c-28.7 0-42.5 16-89.6 16-47.1 0-60.8-16-89.6-16C60.2 288 0 348.2 0 422.4V464c0 26.5 21.5 48 48 48h352c26.5 0 48-21.5 48-48v-41.6c0-74.2-60.2-134.4-134.4-134.4zM416 464c0 8.8-7.2 16-16 16H48c-8.8 0-16-7.2-16-16v-41.6C32 365.9 77.9 320 134.4 320c19.6 0 39.1 16 89.6 16 50.4 0 70-16 89.6-16 56.5 0 102.4 45.9 102.4 102.4V464zM224 256c70.7 0 128-57.3 128-128S294.7 0 224 0 96 57.3 96 128s57.3 128 128 128zm0-224c52.9 0 96 43.1 96 96s-43.1 96-96 96-96-43.1-96-96 43.1-96 96-96z" class=""></path>
-                        </svg>
-                        <span class="text">ออกจากระบบ</span>
-                    </button>
-                </a>
-                <div class="cart">
-                    <a href="{{ url('cart') }}">
-                        <button class="btn-shopping-cart header-menu-item">
-                            <svg aria-hidden="true" focusable="false" data-prefix="fal" data-icon="shopping-cart" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512" class="icon svg-inline--fa fa-shopping-cart fa-w-18">
-                                <path fill="currentColor" d="M551.991 64H129.28l-8.329-44.423C118.822 8.226 108.911 0 97.362 0H12C5.373 0 0 5.373 0 12v8c0 6.627 5.373 12 12 12h78.72l69.927 372.946C150.305 416.314 144 431.42 144 448c0 35.346 28.654 64 64 64s64-28.654 64-64a63.681 63.681 0 0 0-8.583-32h145.167a63.681 63.681 0 0 0-8.583 32c0 35.346 28.654 64 64 64 35.346 0 64-28.654 64-64 0-17.993-7.435-34.24-19.388-45.868C506.022 391.891 496.76 384 485.328 384H189.28l-12-64h331.381c11.368 0 21.177-7.976 23.496-19.105l43.331-208C578.592 77.991 567.215 64 551.991 64zM240 448c0 17.645-14.355 32-32 32s-32-14.355-32-32 14.355-32 32-32 32 14.355 32 32zm224 32c-17.645 0-32-14.355-32-32s14.355-32 32-32 32 14.355 32 32-14.355 32-32 32zm38.156-192H171.28l-36-192h406.876l-40 192z" class=""></path>
-                            </svg>
-                        </button>
-                    </a>
-                </div>
-                @endauth
-                @guest
-                <a href="{{ route('register.perform') }}">
-                    <button class="header-menu-item -with-text">
-                        <svg aria-hidden="true" focusable="false" data-prefix="fal" data-icon="user" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512" class="icon svg-inline--fa fa-user fa-w-14">
-                            <path fill="currentColor" d="M313.6 288c-28.7 0-42.5 16-89.6 16-47.1 0-60.8-16-89.6-16C60.2 288 0 348.2 0 422.4V464c0 26.5 21.5 48 48 48h352c26.5 0 48-21.5 48-48v-41.6c0-74.2-60.2-134.4-134.4-134.4zM416 464c0 8.8-7.2 16-16 16H48c-8.8 0-16-7.2-16-16v-41.6C32 365.9 77.9 320 134.4 320c19.6 0 39.1 16 89.6 16 50.4 0 70-16 89.6-16 56.5 0 102.4 45.9 102.4 102.4V464zM224 256c70.7 0 128-57.3 128-128S294.7 0 224 0 96 57.3 96 128s57.3 128 128 128zm0-224c52.9 0 96 43.1 96 96s-43.1 96-96 96-96-43.1-96-96 43.1-96 96-96z" class=""></path>
-                        </svg>
-                        <span class="text">เข้าสู่ระบบ</span>
-                    </button>
-                </a>
-                <div class="separator"></div>
-                @endguest
-            </section>
+            </form>
+            <div class="text-end">
+                <a href="{{ route('logout.perform') }}" class="btn btn-outline-light me-2">Logout</a>
+            </div>
+            @endauth
+            @guest
+            <div class="text-end">
+                <a href="{{ route('login.perform') }}" class="btn btn-outline-light me-2">Login</a>
+                <a href="{{ route('register.perform') }}" class="btn btn-warning">Sign-up</a>
+            </div>
+            @endguest
         </div>
     </div>
 </header>
