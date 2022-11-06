8f3579960ea7eeca5ac2bdd40a147db3a9e84014
lalinthorn <lalinpye@gmail.com>
2022-11-07 00:51:21 +0700

    css index page

--- database/seeders/ProductSeeder.php
+++ database/seeders/ProductSeeder.php
@@ -24,59 +24,6 @@ class ProductSeeder extends Seeder
             'price' => 4000,
             'MSRP' => 4500
         ],
-        [
-            'categoryID'=> 4,
-            'Pname' => 'headset',
-            'description' => 'headset',
-            'image' => '\assets\image\C_headset.png',
-            'stock' => 999,
-            'price' => 4000,
-            'MSRP' => 4500
-        ],
-        [
-            'categoryID'=> 3,
-            'Pname' => 'keyboard',
-            'description' => 'keyboard',
-            'image' => '\assets\image\C_keyboard.jpg',
-            'stock' => 999,
-            'price' => 4000,
-            'MSRP' => 4500
-        ],
-        [
-            'categoryID'=> 5,
-            'Pname' => 'microphone',
-            'description' => 'microphone',
-            'image' => '\assets\image\C_microphone.jpg',
-            'stock' => 999,
-            'price' => 4000,
-            'MSRP' => 4500
-        ],
-        [
-            'categoryID'=> 1,
-            'Pname' => 'monitor',
-            'description' => 'monitor',
-            'image' => '\assets\image\C_monitor.jpg',
-            'stock' => 999,
-            'price' => 4000,
-            'MSRP' => 4500
-        ],
-        [
-            'categoryID'=> 2,
-            'Pname' => 'mouse',
-            'description' => 'mouse',
-            'image' => '\assets\image\C_mouse.jpg',
-            'stock' => 999,
-            'price' => 4000,
-            'MSRP' => 4500
-        ],[
-            'categoryID'=> 6,
-            'Pname' => 'speaker',
-            'description' => 'speaker',
-            'image' => '\assets\image\C_speaker.jpg',
-            'stock' => 999,
-            'price' => 4000,
-            'MSRP' => 4500
-        ],
         //Product category : Headset
         [
             'categoryID'=> 4,
--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -13,27 +13,17 @@
             line-height: 100px;
             color: #4773EB;
             text-align: center;
-            padding: bottom 15px; ;
+            padding: bottom 15px;
+            ;
         }
 
         .text {
             font-family: 'Didot';
             font-family: 'Prompt', 'Roboto Slab';
             font-size: 20px;
-            font-weight: 300;
+            font-weight: 700;
             line-height: 25px;
 
-        }
-
-        .ttext {
-            font-family: 'PT Serif', serif;
-            font-size: 20px;
-            font-weight: 400;
-            line-height: 30px;
-        }
-
-        .box:hover {
-            box-shadow: 0 0 2px 1px rgba(0, 140, 186, 0.5);
         }
 
         strong {
@@ -54,61 +44,55 @@
             color: rgb(77, 95, 152);
 
         }
-
-        .clearfix::after {
-            content: "";
-            clear: both;
-            display: table;
-        }
     </style>
 </head>
-<div class="p-5 rounded" style="background-color:#eaeaf4 ">
+<div class="bg-light">
     @auth
-    
-    <h1>Products</h1>
-    <p class="lead">Choose the product that you want to buy.</p>
-    <div class="flex-center position-ref full-height">
-        <div class="content">
-            <div class="row">
-                @foreach($products as $product)
-                <div class="col-xs-20 col-sm-6 col-md-5 ">
-                    <div class="clearfix" style="padding: 20px; margin: 25px; border-radius: 8px; border-bottom: 6px solid red; background-color: white; height: max-height; width: 300px; box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);">
-                        <img src="{{ $product->image }}" style="width: 250px;">
-                        <div class="caption">
-                            <h4>{{ $product->Pname }}</h4>
-                            <p class="overflow-md-auto">{{ $product->description }}</p>
-                            <p style="color: Gray; font-size: 18px;"><strong>Amount : </strong>{{ $product->stock }}</p>
-                            <p style="color: Tomato; font-size: 18px;"><strong>Price : </strong> {{ $product->price }} $</p>
-                            <p class="btn-holder"><a href="{{ route('add.to.cart', $product->productCode) }}" class="btn btn-block text-center btn-outline-danger" role="button">Add to cart</a> </p>
-                        </div>
-                    </div>
+    <h1 class="tx-head">category name</h1>
+    <p class="text">Choose the product that you want to buy</p>
+    <div class="row">
+        @foreach($products as $product)
+        <div class="col-sm-6">
+            <div class="card w-0 text-center mx-3 mb-3" style="border-radius: 8px; border-bottom: 6px solid red; box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);">
+                <div class="text-center my-3">
+                    <img src="{{ $product->image }}" style="width: 250px;">
                 </div>
-                @endforeach
+                <div class="card-body">
+                    <h4 class="card-title">{{ $product->Pname }}</h4>
+                    <p class="overflow-md-auto">{{ $product->description }}</p>
+                    <p style="color: Gray; font-size: 18px;"><strong>Amount : </strong>{{ $product->stock }}</p>
+                    <p style="color: Tomato; font-size: 18px;"><strong>Price : </strong> {{ $product->price }} $</p>
+                    <p class="btn-holder"><a href="{{ route('add.to.cart', $product->productCode) }}" class="btn text-center btn-outline-danger" role="button">Add to cart</a> </p>
+
+                </div>
             </div>
         </div>
+        @endforeach
     </div>
-    @endauth
+</div>
+@endauth
+
+<div class="bg-light ">
     @guest
     <h1 class="tx-head">Products</h1>
-    <p class="text" style="text-shadow: 2px 2px red; font-weight: 700;">You have to login before buy the products.</p>
-    <div class="flex-center position-ref full-height">
-        <div class="content">
-            <div class="row">
-            @foreach($products as $product)
-                <div class="col-xs-20 col-sm-6 col-md-5 ">
-                    <div class="clearfix" style="padding: 20px; margin: 25px; border-radius: 8px; border-bottom: 6px solid red; background-color: white; height: max-height; width: 300px; box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);">
-                        <img src="{{ $product->image }}" style="width: 250px;">
-                        <div class="caption">
-                            <h4>{{ $product->Pname }}</h4>
-                            <p class="overflow-md-auto">{{ $product->description }}</p>
-                            <p style="color: Gray; font-size: 18px;"><strong>Amount : </strong>{{ $product->stock }}</p>
-                            <p style="color: Tomato; font-size: 18px;"><strong>Price : </strong> {{ $product->price }} $</p>
-                        </div>
-                    </div>
+    <p class="text" style="text-shadow: 2px 2px red; ">You have to login before buy the products.</p>
+
+    <div class="row">
+        @foreach($products as $product)
+        <div class="col-sm-6">
+            <div class="card w-0 text-center mx-3 mb-3" style="border-radius: 8px; border-bottom: 6px solid red; box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);">
+                <div class="text-center my-3">
+                    <img src="{{ $product->image }}" style="width: 250px;">
                 </div>
-                @endforeach
+                <div class="card-body">
+                    <h4 class="card-title">{{ $product->Pname }}</h4>
+                    <p class="overflow-md-auto">{{ $product->description }}</p>
+                    <p style="color: Gray; font-size: 18px;"><strong>Amount : </strong>{{ $product->stock }}</p>
+                    <p style="color: Tomato; font-size: 18px;"><strong>Price : </strong> {{ $product->price }} $</p>
+                </div>
             </div>
         </div>
+        @endforeach
     </div>
     @endguest
 </div>
